0f39457a5cc2ab6087133e0fc04204df19d300e6
Liberbell <liberbell@gmail.com>
2020-09-13 10:01:07 +0900

    views2

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -3,14 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("""
-                           <!doctype html>
-                           <html>
-                           <head>
-                               <title></title>
-                           </head>
-                           <body>
-                           Welcome to Liber Park
-                           </body>
-                           </html>
-                        """)
+    return render(request, "blog/home.html")
