379f0754cb3d3687146cf55eb46d8a72010486a1
Liberbell <liberbell@gmail.com>
2020-09-20 20:18:37 +0900

    views2

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -3,5 +3,10 @@ from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm()
-    return render(request, 'users/register.html', {'form': form})
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+    else:
+        form = UserCreationForm()
+    # form = UserCreationForm()
+    # return render(request, 'users/register.html', {'form': form})
