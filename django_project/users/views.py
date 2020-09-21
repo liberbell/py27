08e5c692439dc9297a63493e877e9701b6b1d94f
Liberbell <liberbell@gmail.com>
2020-09-21 20:28:21 +0900

    views2

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
@@ -13,6 +13,6 @@ def register(request):
             return redirect('index')
 
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     # form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
