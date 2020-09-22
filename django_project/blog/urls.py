11df7e4296b68fa71a04847d922488d0722f2c6a
Liberbell <liberbell@gmail.com>
2020-09-22 16:53:15 +0900

    urls1

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name='index'),
