a5e1197610b318f6637d3ab0435fb0bbfac19009
Liberbell <liberbell@gmail.com>
2020-09-22 17:05:05 +0900

    urls4

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('business', views.business, name='business'),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
