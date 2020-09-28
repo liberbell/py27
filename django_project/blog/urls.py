eb442692c0497f25e82992d338db08458e1bc917
Liberbell <liberbell@gmail.com>
2020-09-28 23:23:11 +0900

    urls1

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -4,6 +4,7 @@ from users import views as user_views
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import PostListView
 
 urlpatterns = [
     path('', views.index, name='index'),
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -15,6 +15,6 @@ def business(request):
 
 class PostListView(ListView):
     model = Post
-    template_name ='blog/business.html'
+    template_name = 'blog/business.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
