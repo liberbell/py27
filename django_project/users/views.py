8ca631e4cfef66be63d71cdb7aa2109aef5c6000
Liberbell <liberbell@gmail.com>
2020-09-28 22:15:12 +0900

    views3

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -24,6 +24,10 @@ def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
     u_form = UserUpdateForm()
     p_form = ProfileUpdateForm()
     context = {
