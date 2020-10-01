4166badd89ea86f1ce644f58eb6a0d7f106d28d9
Liberbell <liberbell@gmail.com>
2020-10-02 08:47:09 +0900

    views1

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -47,6 +47,7 @@ class PostUpdateView(UserPassesTestMixin, UpdateView):
 
 class PostDeleteView(UserPassesTestMixin, DeleteView):
     model = Post
+    success_url = '/'
 
     def test_func(self):
         post = self.get_object()
